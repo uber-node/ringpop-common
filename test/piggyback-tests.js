9303839f9fc737ae2c29fc1e1943e6321ae11048
Nils Dijk <nils@uber.com>
2015-09-24 13:58:10 +0200

    Ringpop should not start gossiping about information it already knows.

--- test/piggyback-tests.js
+++ test/piggyback-tests.js
@@ -123,3 +123,33 @@ test2('ringpop piggybacking decays', 7, 20000, function(t, tc, n) {
     ];
 });
 
+test2('ringpop piggybacking should ignore updates when it already knows about', 7, 20000, function(t, tc, n) {
+    return [
+        dsl.waitForJoins(t, tc, n),
+        dsl.assertStats(t, tc, n+1, 0, 0),
+        // TODO clear the dissemination information from the SUT by flooding it with pings instead of waiting for it
+        dsl.waitForEmptyPing(t, tc), // problem is that if decay is not working you might never get to this point
+
+        // send information that is already known to the SUT
+        dsl.sendPing(t, tc, 0, {
+            sourceIx: 0,
+            subjectIx: 1,
+            status: 'alive' // this information is already known to the SUT
+        }),
+        dsl.waitForPingResponse(t, tc, 0),
+
+        // TODO speed this up by sending a ping with a correct checksum
+        // Send ping and validate the body
+        dsl.validateEventBody(t, tc, {
+            type: events.Types.Ping,
+            direction: 'request'
+        }, "Test if piggybacked information is not in ping", function (ping) {
+            return !ping.body ||
+                !ping.body.changes ||
+                ping.body.changes.length === 0;
+        }),
+
+        dsl.expectOnlyPings(t, tc),
+    ];
+});
+
