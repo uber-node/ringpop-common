937a7a600191ba30b614a8b1c6b9e0aace7a452e
Nils Dijk <nils@uber.com>
2015-09-24 15:47:01 +0200

    ringpop sends piggyback info in ping-req request

--- test/piggyback-tests.js
+++ test/piggyback-tests.js
@@ -32,37 +32,6 @@ test2('ringpop sends piggyback info in ping request', 7, 20000, function(t, tc, 
     ];
 });
 
-test2('ringpop sends piggyback info in ping response', 7, 20000, function(t, tc, n) {
-    return [
-        dsl.waitForJoins(t, tc, n),
-        dsl.assertStats(t, tc, n+1, 0, 0),
-        // TODO clear the dissemination information from the SUT by flooding it with pings instead of waiting for it
-        dsl.waitForEmptyPing(t, tc), // problem is that if decay is not working you might never get to this point
-
-        // send information to be piggy backed
-        dsl.sendPing(t, tc, 0, {
-            sourceIx: 0,
-            subjectIx: 1,
-            status: 'suspect',
-        }),
-        dsl.waitForPingResponse(t, tc, 0),
-
-        // Send ping and validate the body
-        dsl.sendPing(t, tc, 2),
-        dsl.validateEventBody(t, tc, {
-            type: events.Types.Ping,
-            direction: 'response'
-        }, "Test if piggybacked information is in ping response", function (ping) {
-            return ping.body &&
-                ping.body.changes &&
-                ping.body.changes.length == 1 &&
-                ping.body.changes[0] &&
-                ping.body.changes[0].source == tc.fakeNodes[0].getHostPort() &&
-                ping.body.changes[0].status == "suspect";
-        })
-    ];
-});
-
 test2('ringpop updates its dissimination list on pingreq', 7, 20000, function(t, tc, n) {
     return [
         dsl.waitForJoins(t, tc, n),
@@ -171,5 +140,72 @@ test2('ringpop piggybacking should ignore updates when it already knows about', 
                 !ping.body.changes ||
                 ping.body.changes.length === 0;
         })
+    ];
+});
+
+test2('ringpop sends piggyback info in ping response', 7, 20000, function(t, tc, n) {
+    return [
+        dsl.waitForJoins(t, tc, n),
+        dsl.assertStats(t, tc, n+1, 0, 0),
+        // TODO clear the dissemination information from the SUT by flooding it with pings instead of waiting for it
+        dsl.waitForEmptyPing(t, tc), // problem is that if decay is not working you might never get to this point
+
+        // send information to be piggy backed
+        dsl.sendPing(t, tc, 0, {
+            sourceIx: 0,
+            subjectIx: 1,
+            status: 'suspect',
+        }),
+        dsl.waitForPingResponse(t, tc, 0),
+
+        // Send ping and validate the body
+        dsl.sendPing(t, tc, 2),
+        dsl.validateEventBody(t, tc, {
+            type: events.Types.Ping,
+            direction: 'response'
+        }, "Test if piggybacked information is in ping response", function (ping) {
+            return ping.body &&
+                ping.body.changes &&
+                ping.body.changes.length == 1 &&
+                ping.body.changes[0] &&
+                ping.body.changes[0].source == tc.fakeNodes[0].getHostPort() &&
+                ping.body.changes[0].status == "suspect";
+        })
+    ];
+});
+
+test2('ringpop sends piggyback info in ping-req request', 7, 20000, function(t, tc, n) {
+    return [
+        dsl.waitForJoins(t, tc, n),
+        dsl.assertStats(t, tc, n+1, 0, 0),
+        // TODO clear the dissemination information from the SUT by flooding it with pings instead of waiting for it
+        dsl.waitForEmptyPing(t, tc), // problem is that if decay is not working you might never get to this point
+
+        // send information to be piggy backed
+        dsl.sendPing(t, tc, 0, {
+            sourceIx: 0,
+            subjectIx: 1,
+            status: 'suspect',
+        }),
+        dsl.waitForPingResponse(t, tc, 0),
+
+        // cause the SUT to send a ping-req
+        dsl.disableNode(t, tc, 1),
+
+        // Wait for a ping from the SUT and validate that it has the piggybacked information in there
+        dsl.validateEventBody(t, tc, {
+            type: events.Types.PingReq,
+            direction: 'request'
+        }, "Test if piggybacked information is in ping", function (ping) {
+            return ping.body &&
+                ping.body.changes &&
+                ping.body.changes.length == 1 &&
+                ping.body.changes[0] &&
+                ping.body.changes[0].source == tc.fakeNodes[0].getHostPort() &&
+                ping.body.changes[0].status == "suspect";
+        }),
+
+        // consume other pingreq's
+        dsl.waitForPingReqs(t, tc, 2),
     ];
 });
