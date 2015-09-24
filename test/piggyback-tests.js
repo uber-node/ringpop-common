b28f0c343e34465f9de6d3ecab01f91b6dde68a5
Nils Dijk <nils@uber.com>
2015-09-24 15:37:30 +0200

    ringpop sends piggyback info in ping-req response.

--- test/piggyback-tests.js
+++ test/piggyback-tests.js
@@ -93,6 +93,38 @@ test2('ringpop updates its dissimination list on pingreq', 7, 20000, function(t,
     ];
 });
 
+test2('ringpop sends piggyback info in ping-req response', 7, 20000, function(t, tc, n) {
+    return [
+        dsl.waitForJoins(t, tc, n),
+        dsl.assertStats(t, tc, n+1, 0, 0),
+        // TODO clear the dissemination information from the SUT by flooding it with pings instead of waiting for it
+        dsl.waitForEmptyPing(t, tc), // problem is that if decay is not working you might never get to this point
+
+        // send information to be piggy backed
+        dsl.sendPing(t, tc, 0, {
+            sourceIx: 0,
+            subjectIx: 1,
+            status: 'suspect',
+        }),
+        dsl.waitForPingResponse(t, tc, 0),
+
+        dsl.sendPingReq(t, tc, 1, 2),
+
+        // Wait for a ping from the SUT and validate that it has the piggybacked information in there
+        dsl.validateEventBody(t, tc, {
+            type: events.Types.PingReq,
+            direction: 'response'
+        }, "Test if piggybacked information is in pingreq response", function (ping) {
+            return ping.body &&
+                ping.body.changes &&
+                ping.body.changes.length == 1 &&
+                ping.body.changes[0] &&
+                ping.body.changes[0].source == tc.fakeNodes[0].getHostPort() &&
+                ping.body.changes[0].status == "suspect";
+        })
+    ];
+});
+
 test2('ringpop piggybacking decays', 7, 20000, function(t, tc, n) {
     return [
         dsl.waitForJoins(t, tc, n),
@@ -141,4 +173,3 @@ test2('ringpop piggybacking should ignore updates when it already knows about', 
         })
     ];
 });
-
