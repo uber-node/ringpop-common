55d8d4eb563a18a7f4cd394a9bbfdbc8e509fcea
Menno Pruijssers <menno@uber.com>
2016-04-20 12:13:27 +0200

    Clean up of bi-dir. sync throttling test

--- test/bidir-full-sync-tests.js
+++ test/bidir-full-sync-tests.js
@@ -48,74 +48,67 @@ test2('bidirectional full sync test', getClusterSizes(5), 20000,
 	})
 );
 
+// As a safety check, both the ringpop-go and ringpop-node limit the number
+// (defaults to 5) of concurrent bidirectional full syncs.
+//
+// A bidirectional full sync is triggered by a ping to a node without outstanding
+// membership changes where the checksum in the ping doesn't match the node's checksum.
+// This results in the node sending it's full membership. At the same time, the node
+// will send a join to the source of the ping request to get it's full membership list.
+//
+// This test pauses responding to the join request to make sure there are multiple
+// joins happening at the same time. If the throttling is working, there should
+// never be more than 5 joins in the queue.
 test2('bidirectional full sync throttling test', getClusterSizes(3), 20000,
     prepareCluster(function(t, tc, n) {
+        var numberOfFullSyncsToTrigger = Math.max(6, n*2);
+        var expectedJoins = 5;
         return [
             dsl.assertStats(t, tc, n+1, 0, 0),
             dsl.waitForEmptyPing(t, tc),
 
-            assertReverseFullSyncThrottling(t, tc, 0, n*3, 5),
+            //pause handling joins to stall reverse full syncs
+            overwriteJoinHandlerWithPause(tc, 0),
 
-            dsl.assertStats(t, tc, n+1, 0, 0),
+            // trigger the reverse full syncs
+            triggerReverseFullSyncs(tc, 0, numberOfFullSyncsToTrigger),
+
+            // verify the number of )cached) join requests
+            verifyCachedJoins(t, tc, 0, numberOfFullSyncsToTrigger, expectedJoins),
+
+            // handle the joins and restore the original join-handler.
+            handleCachedJoinsAndRestoreJoinHandler(tc, 0),
+
+            // wait for the joins to be handled.
+            dsl.waitForJoins(t, tc, expectedJoins),
+
+            dsl.assertStats(t, tc, n+1, 0, 0)
         ];
     })
 );
 
-// assert if the number of joins is throttled.
-function assertReverseFullSyncThrottling(t, tc, idx, count, expectedJoins) {
-    var functions = [];
-
-    //pause handling joins to stall reverse full syncs
-    functions.push(pauseHandlingJoins);
-    functions.push(dsl.waitForEmptyPing(t, tc));
-
-    // trigger the reverse full syncs
-    functions.push(triggerReverseFullSyncs(tc, idx, count));
-
-    // verify the number of join requests
-    functions.push(verify);
-
-    // handle the joins
-    functions.push(resumeHandlingJoins);
-
-    // the SUT sends a join request to perform a bidirectional full sync
-    functions.push(dsl.waitForJoins(t, tc, expectedJoins));
-
-    var joins = [];
-
-    return functions;
-
-    function triggerReverseFullSyncs(tc, nodeIx, count) {
-        var f = function triggerFullSync(list, cb) {
-            async.times(count, function triggerFullSync(i, next){
-                tc.fakeNodes[nodeIx].requestPing(next, undefined, {checksum: 1}); //override checksum to trigger a fullsync
-            }, function done() {
-                cb(list);
-            });
-        };
-        f.callerName = 'triggerReverseFullSyncs';
-
-        return f;
-    }
-
-    function pauseHandlingJoins(list, cb) {
+function overwriteJoinHandlerWithPause(tc, idx) {
+    return function overwriteJoinHandlerWithPause(list, cb) {
         var fakeNode = tc.fakeNodes[idx];
 
+        fakeNode.cachedJoins = [];
         fakeNode.endpoints['Join'].handler = function() {
-            console.log('handling join');
-            joins.push(arguments)
+            fakeNode.cachedJoins.push(arguments)
         };
-
 
         cb(list);
     }
+}
 
-    function verify(list, cb) {
-        if (joins.length < expectedJoins) {
+function verifyCachedJoins(t, tc, idx, fullSyncs, expectedJoins) {
+    return function verifyCachedJoins(list, cb) {
+        var fakeNode = tc.fakeNodes[idx];
+        var cachedJoins = fakeNode.cachedJoins;
+        if (cachedJoins.length < expectedJoins) {
             cb(null);
             return;
         }
-        t.equal(joins.length, expectedJoins);
+        t.equal(cachedJoins.length, expectedJoins);
 
         var pings = _.filter(list, {
             type: events.Types.Ping,
@@ -126,23 +119,40 @@ function assertReverseFullSyncThrottling(t, tc, idx, count, expectedJoins) {
             return event.receiver === tc.fakeNodes[idx].getHostPort();
         });
 
-        if (pings.length < count) {
+        if (pings.length < fullSyncs) {
             cb(null);
             return;
         }
 
-        t.equals(pings.length, count);
+        t.equals(pings.length, fullSyncs);
 
         cb(_.reject(list, {type: events.Types.Ping, direction: 'response'}));
     }
+}
 
-    function resumeHandlingJoins(list, cb) {
+function triggerReverseFullSyncs(tc, nodeIx, count) {
+    var f = function triggerFullSync(list, cb) {
+        async.times(count, function triggerFullSync(i, next){
+            tc.fakeNodes[nodeIx].requestPing(next, undefined, {checksum: 1}); //override checksum to trigger a fullsync
+        }, function done() {
+            cb(list);
+        });
+    };
+    f.callerName = 'triggerReverseFullSyncs';
+
+    return f;
+}
+
+function handleCachedJoinsAndRestoreJoinHandler(tc, idx) {
+    return function handleCachedJoinsAndRestoreJoinHandler(list, cb) {
         var fakeNode = tc.fakeNodes[idx];
 
         fakeNode.endpoints['Join'].handler = fakeNode.joinHandler;
 
-        for(var i=0; i< joins.length; i++) {
-            fakeNode.joinHandler.apply(fakeNode, joins[i]);
+        var cachedJoins = fakeNode.cachedJoins;
+
+        for(var i=0; i< cachedJoins.length; i++) {
+            fakeNode.joinHandler.apply(fakeNode, cachedJoins[i]);
         }
 
         cb(list);
